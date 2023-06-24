0614b6b0b5f312d250734a9a83e2db7f5d852d03
Tyler Hunt <tylerandrewhunt96@gmail.com>
2023-06-24 19:23:30 -0400

    Run expectancy matrix complete for test matrix. RE Matrix CSV file as output

--- run_expectancy.R
+++ run_expectancy.R
@@ -3,6 +3,7 @@
 
 ## Read in the necessary libararies
 library("dplyr")
+library("tidyverse")
 
 ## Set working directory and read in PBP CSV
 setwd('C:/Users/tyler/OneDrive/Coding Work Materials/ncaa_run_expectancies')
@@ -10,3 +11,23 @@ pbp = read.csv('single_pbp.csv',header = TRUE)
 
 ## Next steps
 ## Partition data by base-out state and AVERAGE runs til end of inning attributes
+
+re_matrix <- pbp %>%
+  mutate(runs_end_2 = end_half_inning_runs - current_runs) %>%
+  group_by(base_cd_before,outs_before) %>%
+  summarize(mean = mean(runs_end_2))
+
+View(re_matrix)
+
+## Test a base/out state to validate results. Can do manual calculations if small enough dataset
+
+testing <- filter(pbp, base_cd_before == 5 & outs_before == 0)
+
+View(testing)
+
+## Write final results to a CSV file
+
+write.csv(x = re_matrix,
+          file = "RE_Matrix.csv",
+          row.names = FALSE)
+
