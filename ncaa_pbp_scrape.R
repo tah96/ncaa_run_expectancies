dd73a46f14a114df9ed2dfdca8db11320bc9bb8a
Tyler Hunt <tylerandrewhunt96@gmail.com>
2023-07-03 12:36:47 -0400

    Retrieved all distinct boxscore URLs. Stored in bscore.csv to reduce work

--- ncaa_pbp_scrape.R
+++ ncaa_pbp_scrape.R
@@ -1,26 +1,44 @@
 library("baseballr")
 library("dplyr")
 library("tidyverse")
+library("glue")
 
 setwd('C:/Users/tyler/OneDrive/Coding Work Materials/ncaa_run_expectancies')
 
 all_schools <- ncaa_school_id_lu(team_name = "")
 d1_schools <- filter(all_schools, division == 1 & year == 2023)
 all_team_ids <- sort(unique(d1_schools$team_id), decreasing = FALSE)
+next_run <- all_team_ids[all_team_ids > 458]
 
-all_team_ids
+boston <- filter(all_schools, team_name == 'Boston College')
+
+#all_team_ids
+bscore_vec <- c()
 
 pbp_scrape <- function(team_id) {
-  bscore_vec <- c()
   y <- ncaa_schedule_info(team_id, 2023)
   bx_urls <- y$game_info_url
   for (i in seq_along(bx_urls)) {
     url = unlist(bx_urls[i])
-    if (any(url==bscore_vec) == FALSE) {
-      bscore_vec <- append(bscore_vec,url)
+    if (!is.na(url)) {
+      if (any(url==bscore_vec) == FALSE) {
+        bscore_vec <<- append(bscore_vec,url)
+      }
     }
   }
-  print(bscore_vec)
+  print(glue("Done scraping {team_id}"))
+  return("Finished")
 }
 
-pbp_scrape(490)
+for (team in next_run) {
+  pbp_scrape(team)
+}
+
+##pbp_scrape(368)
+
+##length(bscore_vec)
+##length(unique(bscore_vec))
+
+write.csv(x = bscore_vec,
+          file = "bscore_list.csv",
+          row.names = FALSE)
