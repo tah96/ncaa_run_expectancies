b23338a2ee886978097829cc40e848bc4eae2022
Tyler Hunt <tylerandrewhunt96@gmail.com>
2023-07-04 13:07:14 -0400

    ncaa_pbp_scrape bad scraping adjustment. First 1939 games fetched. Stored outside repository

--- ncaa_pbp_scrape.R
+++ ncaa_pbp_scrape.R
@@ -8,13 +8,26 @@ blist = read.csv('bscore_list.csv',header = TRUE)
 
 blist_vector <- blist$x
 
+total_boxscores <- length(blist_vector)
 
-pbp <- ncaa_pbp(game_info_url = blist_vector[1])
+pbp <- ncaa_pbp(game_info_url = blist_vector[1940])
 
 ## will use length(blist_vector)
 
-for (i in 2:6) {
+for (i in 1941:total_boxscores) {
   game_pbp <- ncaa_pbp(game_info_url = blist_vector[i])
-  pbp <<- rbind(pbp,game_pbp)
+  pbp <<- rbind(pbp,game_pbp,use.names=TRUE,fill=TRUE)
   print(i)
 }
+
+View(pbp)
+
+pbp_qa <- ncaa_pbp(game_info_url = blist_vector[149])
+
+View(pbp_qa)
+
+setwd('C:/Users/tyler/OneDrive/Coding Work Materials')
+
+write.csv(x = pbp,
+          file = "pbp_1939.csv",
+          row.names = FALSE)
