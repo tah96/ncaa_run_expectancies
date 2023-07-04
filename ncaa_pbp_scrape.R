3515095a9fe3cc81b3a6cbc246fe4475ac57f672
Tyler Hunt <tylerandrewhunt96@gmail.com>
2023-07-04 19:25:42 -0400

    PBP scraping up to 3500

--- ncaa_pbp_scrape.R
+++ ncaa_pbp_scrape.R
@@ -10,11 +10,11 @@ blist_vector <- blist$x
 
 total_boxscores <- length(blist_vector)
 
-pbp <- ncaa_pbp(game_info_url = blist_vector[1940])
+pbp <- ncaa_pbp(game_info_url = blist_vector[3501])
 
 ## will use length(blist_vector)
 
-for (i in 1941:total_boxscores) {
+for (i in 3502:total_boxscores) {
   game_pbp <- ncaa_pbp(game_info_url = blist_vector[i])
   pbp <<- rbind(pbp,game_pbp,use.names=TRUE,fill=TRUE)
   print(i)
@@ -29,5 +29,5 @@ View(pbp_qa)
 setwd('C:/Users/tyler/OneDrive/Coding Work Materials')
 
 write.csv(x = pbp,
-          file = "pbp_1939.csv",
+          file = "pbp_3500.csv",
           row.names = FALSE)
