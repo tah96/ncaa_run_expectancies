757e30f9db55d55cdc9893ff1bf0b6cab6022d0d
Tyler Hunt <tylerandrewhunt96@gmail.com>
2023-07-15 21:54:16 -0400

    Started scraping script for individual player season stats across conferences

---
+++ indiv_stat_scrape.R
@@ -0,0 +1,28 @@
+install.packages("rvest")
+library(rvest)
+
+standard_links <- c('https://theacc.com/stats.aspx?path=baseball&year=2023#individual',
+           'https://bigten.org/stats.aspx?path=baseball&year=2023#individual')
+
+ncaa_bat_stats = data.frame()
+ncaa_pit_stats = data.frame()
+
+for (i in standard_links) {
+  page <- read_html(i)
+  
+  stats_bat <- page %>%
+    html_node('#ind_hitting') %>%
+    html_element("table") %>%
+    html_table()
+  
+  stats_pit <- page %>%
+    html_node('#ind_pitching') %>%
+    html_element("table") %>%
+    html_table()
+  
+  bat_df <- data.frame(stats_bat)
+  pit_df <- data.frame(stats_pit)
+  
+  ncaa_bat_stats <<- rbind(ncaa_bat_stats,bat_df,use.names=TRUE,fill=TRUE)
+  ncaa_pit_stats <<- rbind(ncaa_pit_stats,pit_df,use.names=TRUE,fill=TRUE)
+}
