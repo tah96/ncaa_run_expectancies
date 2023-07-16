5b1165b8beada6eacde2cc628d9003af7fc7c1ff
Tyler Hunt <tylerandrewhunt96@gmail.com>
2023-07-16 11:54:20 -0400

    All conferences with URLs that follow standardized HTML included. Test ran

--- indiv_stat_scrape.R
+++ indiv_stat_scrape.R
@@ -1,8 +1,35 @@
 install.packages("rvest")
 library(rvest)
 
+### Following conferences missing from below ###
+### PAC-12, SEC, SOCON ###
+
 standard_links <- c('https://theacc.com/stats.aspx?path=baseball&year=2023#individual',
-           'https://bigten.org/stats.aspx?path=baseball&year=2023#individual')
+           'https://bigten.org/stats.aspx?path=baseball&year=2023#individual',
+           'https://theamerican.org/stats.aspx?path=baseball&year=2023',
+           'https://americaeast.com/stats.aspx?path=baseball&year=2023&',
+           'https://atlantic10.com/stats.aspx?path=baseball&year=2023',
+           'https://asunsports.org/stats.aspx?path=baseball&year=2023',
+           'https://big12sports.com/stats.aspx?path=baseball&year=2023',
+           'https://www.bigeast.com/stats.aspx?path=baseball&year=2023',
+           'https://bigsouthsports.com/stats.aspx?path=baseball&year=2023',
+           'https://bigwest.org/stats.aspx?path=baseball&year=2023',
+           'https://caasports.com/stats.aspx?path=baseball&year=2023',
+           'https://conferenceusa.com/stats.aspx?path=baseball&year=2023',
+           'https://horizonleague.org/stats.aspx?path=baseball&year=2023',
+           'https://ivyleague.com/stats.aspx?path=baseball&year=2023',
+           'https://maacsports.com/stats.aspx?path=baseball&year=2023',
+           'https://getsomemaction.com/stats.aspx?path=baseball&year=2023',
+           'https://mvc-sports.com/stats.aspx?path=baseball&year=2023',
+           'https://themw.com/stats.aspx?path=baseball&year=2023',
+           'https://northeastconference.org/stats.aspx?path=baseball&year=2023',
+           'https://ovcsports.com/stats.aspx?path=baseball&year=2023',
+           'https://patriotleague.org/stats.aspx?path=baseball&year=2023',
+           'https://www.southland.org/stats.aspx?path=baseball&year=2023',
+           'https://swac.org/stats.aspx?path=baseball&year=2023',
+           'https://thesummitleague.org/stats.aspx?path=baseball&year=2023',
+           'https://sunbeltsports.org/stats.aspx?path=baseball&year=2023',
+           'https://wccsports.com/stats.aspx?path=baseball&year=2023')
 
 ncaa_bat_stats = data.frame()
 ncaa_pit_stats = data.frame()
